02be8652798aa7b49bd8b1d56c6a4fb4e5287075
GessicaGobbi <gessicagobbi@hotmail.com>
2025-03-31 15:38:21 -0300

    1-2

--- src/implementacao/1-numeros-0-a-40.ts
+++ src/implementacao/1-numeros-0-a-40.ts
@@ -1,3 +1,16 @@
-function mostrarNumeros() {
-    return null;
+function exibirNumerosAte40() {
+    for(let i:number=0;i<=40;i++)
+        console.log(i);
 }
+module.exports =exibirNumerosAte40;
+
+exibirNumerosAte40();
+
+/**
+ * Implemente o método exibirNumerosAte40, que exibe todos os números de 0 a 40.
+
+O método deve iterar de 0 a 40 e exibir cada número na sequência.
+Exemplos:
+Quando o método for chamado, a saída deve ser: 0, 1, 2, 3, ..., 40.
+
+ */
--- src/implementacao/2-numeros-0-a-n.ts
+++ src/implementacao/2-numeros-0-a-n.ts
@@ -1,3 +1,13 @@
 function mostrarNumerosAteN() {
     return null;
 }
+/**
+ * Implemente o método exibirNumerosAteN, que recebe um número N e exibe todos os números de 0 até N.
+
+O método deve iterar de 0 até o valor de N e exibir cada número.
+Exemplos:
+Para a entrada N = 5, a saída deve ser: 0, 1, 2, 3, 4, 5.
+Para a entrada N = 10, a saída deve ser: 0, 1, 2, 3, ..., 10.
+Para a entrada N = 3, a saída deve ser: 0, 1, 2, 3.
+
+ */
